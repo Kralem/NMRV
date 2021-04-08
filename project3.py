fcc5218b3656d9d42375cc53747626b2f7ea328f
Mateo Kalem <45400194+Kralem@users.noreply.github.com>
2021-04-08 12:46:05 +0200

    Add files via upload

--- project3.py
+++ project3.py
@@ -10,7 +10,8 @@ class Project3Tracker(Tracker):
         self.region = region
         self.window = max(region[2], region[3]) * self.parameters.enlarge_factor
         self.position = (region[0] + region[2] / 2, region[1] + region[3] / 2)
-        self.G = create_gauss_peak((self.size[1], self.size[0]), 1)
+
+        self.G = create_gauss_peak((self.size[0], self.size[1]), 3)
         self.patch, inliers = get_patch(image, self.position, self.G.shape[::-1])
         self.cosine = create_cosine_window(self.patch.shape[::-1])
         self.patch = np.multiply(self.cosine, self.patch)
@@ -22,28 +23,31 @@ class Project3Tracker(Tracker):
 
     def track(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        patch_n, inliers = get_patch(image, self.position, self.G.shape[::-1])
-        cosine_n = create_cosine_window(patch_n.shape[::-1])
-        patch_n = np.multiply(patch_n, cosine_n)
-        oblika = patch_n.shape[::-1]
-        H_n = np.divide(np.multiply(np.fft.fft2(self.G), np.conj(np.fft.fft2(patch_n))),
-                           (np.multiply(np.fft.fft2(patch_n), np.conj(np.fft.fft2(patch_n))) + 0.000005))
-        H_update = (1 - 0.9)*self.H + 0.9*H_n
-        R = np.fft.ifft2(np.multiply(H_update, np.fft.fft2(patch_n)))
-        R_max = np.max(R)
-        pozicija = np.where(R == R_max)
-        print(pozicija)
-        kx = pozicija[1][0]
-        ky = pozicija[0][0]
+        oblika = self.G.shape[::-1]
+        patch, inliers = get_patch(image, self.position, self.G.shape[::-1])
+        patch = np.multiply(self.cosine, patch)
+        R = np.real(np.fft.ifft2(np.multiply(self.H, np.fft.fft2(patch))))
+        #cv2.imshow('okno',R)
+        #cv2.waitKey(0)
+        ind = np.unravel_index(np.argmax(R, axis=None), R.shape)
+
+        kx = ind[1]
+        ky = ind[0]
         if kx > (self.size[0] / 2):
-            x = kx - self.size[0]
-            kx = kx + x
+            kx = kx - self.size[0]
         if ky > (self.size[1] / 2):
-            y = kx - self.size[1]
-            ky = ky + y
+            ky = ky - self.size[1]
 
 
-        return [kx, ky, self.size[0], self.size[1]]
+        self.position = (self.position[0]+kx, self.position[1]+ky)
+        patch_n, inliers = get_patch(image, self.position, self.G.shape[::-1])
+        patch_n = np.multiply(patch_n, self.cosine)
+        H_n = np.divide(np.multiply(np.fft.fft2(self.G), np.conj(np.fft.fft2(patch_n))),
+                        (np.multiply(np.fft.fft2(patch_n), np.conj(np.fft.fft2(patch_n))) + 0.000005))
+        self.H = (1 - 0.15) * self.H + 0.15 * H_n
+
+
+        return [self.position[0] - (self.size[0] /2), self.position[1] - (self.size[1] / 2), self.size[0], self.size[1]]
 
 
 class Project3Params():
