ec7a4a599e68142a93f11b39bdc858c86e19e251
Mateo Kalem <45400194+Kralem@users.noreply.github.com>
2021-04-12 18:51:16 +0200

    Add files via upload

--- tracker_time.py
+++ tracker_time.py
@@ -1,18 +1,22 @@
-from utils.tracker import Tracker
+from ex2_utils import Tracker
 from ex2_utils import get_patch
 from ex3_utils import create_gauss_peak, create_cosine_window
 import numpy as np
 import cv2
+import time
 
 class Project3Tracker(Tracker):
     def name(self):
         return 'Project3Tracker'
 
     def initialize(self, image, region):
+        self.fps = 0
+        self.no_frames = 0
+        t0 = time.time()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         self.size = (region[2], region[3])
         self.region = region
-        self.window = max(region[2], region[3]) * 2
+        self.window = max(region[2], region[3]) * self.parameters.enlarge_factor
         self.position = (region[0] + region[2] / 2, region[1] + region[3] / 2)
 
         self.G = create_gauss_peak((self.size[0], self.size[1]), 1.75)
@@ -24,8 +28,15 @@ class Project3Tracker(Tracker):
         #print(np.shape(self.G))
         self.H = np.divide(np.multiply(np.fft.fft2(self.G), np.conj(np.fft.fft2(self.patch))),
                            (np.multiply(np.fft.fft2(self.patch), np.conj(np.fft.fft2(self.patch))) + 0.000005))
+        t1 = time.time()
+        if (t1 - t0) != 0:
+            self.fps = self.fps + (1 / (t1 - t0))
+            self.no_frames += 1
+            print("total time for initializing tracker", self.fps)
+        self.fps = 0
 
     def track(self, image):
+        t0 = time.time()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         oblika = self.G.shape[::-1]
         patch, inliers = get_patch(image, self.position, self.G.shape[::-1])
@@ -50,11 +61,18 @@ class Project3Tracker(Tracker):
                         (np.multiply(np.fft.fft2(patch_n), np.conj(np.fft.fft2(patch_n))) + 0.000005))
         self.H = (1 - 0.06) * self.H + 0.06 * H_n
 
+        t1 = time.time()
+        if (t1 - t0) != 0:
+            self.fps = self.fps + (1 / (t1 - t0))
+            self.no_frames += 1
+            print("total time for updating tracker", self.fps)
+            print("processed frames", self.no_frames)
+
 
         return [self.position[0] - (self.size[0] /2), self.position[1] - (self.size[1] / 2), self.size[0], self.size[1]]
 
 
-#class Project3Params():
-#    def __init__(self):
-#        self.enlarge_factor = 2
+class Project3Params():
+    def __init__(self):
+        self.enlarge_factor = 2
 
